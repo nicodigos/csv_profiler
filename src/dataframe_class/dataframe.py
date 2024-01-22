e6bc4971ecd9ccc9187488a16f812b6d158708d3
Nicolas Peralta Paez <c0932238@mylambton.ca>
2024-01-22 12:42:18 -0500

    pie chart added

--- src/dataframe_class/dataframe.py
+++ src/dataframe_class/dataframe.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import random
 
-colors_list = ['indianred', 'yellow']
+colors_list = ['indianred']
 
 class DataFrameGeneral():
 
@@ -25,7 +25,22 @@ class DataFrameGeneral():
                 visible=False, showticklabels=False).update_layout(margin=dict(l=0, r=0, t=0, b=0),paper_bgcolor='rgba(0,0,0,0)',
                 plot_bgcolor='rgba(0,0,0,0)')
                 attributes_dict[i].update({"histogram":plotly_figure})
-        self.profiler = attributes_dict
+            else:
+                values_counter = df_1[i].value_counts() / df_1[i].notnull().sum()
+                values_counter = values_counter.to_frame().reset_index().rename(columns={'count':'value'})
+                other_sum = values_counter[values_counter[values_counter.columns[1]] < 0.1][values_counter.columns[1]].sum()
+                chart_df = values_counter[values_counter['value'] > 0.1]
+                if chart_df.shape[0] != 0:
+                    if other_sum > 0:
+                        append_df = pd.DataFrame([['Other Values', other_sum]], columns=[chart_df.columns[0],chart_df.columns[1]])
+                        chart_df = pd.concat([chart_df, append_df])
+                    attributes_dict[i].update({"histogram_table": chart_df})
+                    plotly_figure = px.pie(chart_df, values=chart_df.columns[1], names=chart_df.columns[0],
+                                        template='simple_white',width=300, height=200)
+                    plotly_figure.update_layout(margin=dict(l=0, r=0, t=0, b=0),paper_bgcolor='rgba(0,0,0,0)',
+                    )
+                    attributes_dict[i].update({"histogram":plotly_figure})
+                self.profiler = attributes_dict
 
 class DataFrameModif(DataFrameGeneral):
     pass
